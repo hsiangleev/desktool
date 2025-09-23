e9a42ac4d8634999df50d904b33a44867f9d455c
李祥 <1547600955@qq.com>
2025-09-23 08:55:13 +0800

    首页修改

--- src/views/dashboard/workspace.vue
+++ src/views/dashboard/workspace.vue
@@ -1,7 +1,20 @@
 <template>
-    <el-empty :description='text' />
+    <div class='el-empty'>
+        <div class='el-empty__image'>😁</div>
+        <div class='el-empty__description'><p>{{ text }}</p></div>
+    </div>
 </template>
 <script setup lang='ts'>
-const text = ref('心有猛虎，细嗅蔷薇')
-fetch('https://v1.hitokoto.cn/').then(async(d) => text.value = (await d.json()).hitokoto)
+const text = ref('')
+fetch('https://v1.hitokoto.cn/')
+    .then(async(d) => text.value = (await d.json()).hitokoto)
+    .catch(() => text.value = '我有故人抱剑去，斩尽春风未曾归。')
 </script>
+
+<style scoped>
+    .el-empty__image{
+        margin: 50px 0 10px;
+        font-size: 100px;
+        opacity: 0.2;
+    }
+</style>
